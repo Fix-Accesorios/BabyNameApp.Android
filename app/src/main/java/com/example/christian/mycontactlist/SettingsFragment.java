6d53af37ea08b4ef10cc66b331bf20a90ffa947e
Christian Matsoukis <matsouki@msu.edu>
2018-06-21 17:06:10 -0400

    got the json array request working. sample in setting gragment java class.

--- app/src/main/java/com/example/christian/mycontactlist/SettingsFragment.java
+++ app/src/main/java/com/example/christian/mycontactlist/SettingsFragment.java
@@ -11,10 +11,12 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.w3c.dom.Text;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * A fragment representing a list of Items.
@@ -22,29 +24,55 @@ import org.w3c.dom.Text;
  */
 public class SettingsFragment extends Fragment {
     private TextView settingTextView;
-    private String url ="http://www.google.com";
+    private String url = "http:/35.9.99.211:8081/names/1/100/";
+    // Request a string response from the provided URL.
+    JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+            Request.Method.GET,
+            url,
+            null,
+            new Response.Listener<JSONArray>() {
+                @Override
+                public void onResponse(JSONArray response) {
+                    // Do something with response
+                    //mTextView.setText(response.toString());
+
+                    // Process the JSON
+                    try {
+                        // Loop through the array elements
+                        for (int i = 0; i < response.length(); i++) {
+                            // Get current json object
+                            JSONObject name_obj = response.getJSONObject(i);
+
+                            // Get the current student (json object) data
+                            String name = name_obj.getString("name");
+                            String gender = (name_obj.getInt("gender_id") == 1) ? "Boy" : "Female";
+
+                            // Display the formatted json data in text view
+                            settingTextView.append(name + " - " + gender);
+                            settingTextView.append("\n");
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            },
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    // Do something when error occurred
+                    settingTextView.setText("This did not work.");
+                }
+            }
+    );
     // Instantiate the RequestQueue.
     private RequestQueue queue;
-    // Request a string response from the provided URL.
-    StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-            new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    // Display the first 500 characters of the response string.
-                    settingTextView.setText("Response is: "+ response.substring(0,500));
-                }
-            }, new Response.ErrorListener() {
-        @Override
-        public void onErrorResponse(VolleyError error) {
-            settingTextView.setText("That didn't work!");
-        }
-    });
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.settings_fragment, container, false);
         queue = Volley.newRequestQueue(this.getContext());
         settingTextView = (TextView) view.findViewById(R.id.settingTextView);
-        queue.add(stringRequest);
+        queue.add(jsonArrayRequest);
         return view;
     }
 }
